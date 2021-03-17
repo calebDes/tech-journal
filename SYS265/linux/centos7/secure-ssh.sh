ce4214a36d36f65663e4b53eb000fd9ddc0e13c2
root <root@web01-caleb.caleb.local>
2021-03-17 16:18:18 -0400

    Committer: root <root@web01-caleb.caleb.local>

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+# secure-ssh.sh
+# author caleb
+# creates new ssh parameter using $1 
+# adds a public key or is curled from the repo
+# removes roots ability to ssh in
+echo "all your code goes here"
